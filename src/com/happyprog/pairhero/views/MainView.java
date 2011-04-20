35f083607812d2abad069e208769aff140b377c2
Sebastian Hermida <sebastian@sbastn.com>
2011-04-20 17:38:53 -0400

    Adding skeleton app

---
+++ src/com/happyprog/pairhero/views/MainView.java
@@ -0,0 +1,22 @@
+package com.happyprog.pairhero.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MainView extends ViewPart {
+
+	public static final String ID = "com.happyprog.pairhero.views.MainView";
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
